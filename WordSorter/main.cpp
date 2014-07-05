68baef9126a41c39e1123589ab8c8eb1c710748f
Pierre WILMOT <pierre.wilmot@gmail.com>
2014-07-05 15:15:27 +1000

    Initial Commit

---
+++ WordSorter/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  WordSorter
+//
+//  Created by Pierre WILMOT on 5/07/2014.
+//  Copyright (c) 2014 WILMOT. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
